cb5ad1d7415a29fe01da43f1f652b3aa98c2b04a
davitshavidze <onlygameing61@gmail.com>
2024-11-01 19:40:56 +0400

    Third Commit

---
+++ Level 7/ClassWork/Class.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+name = input('Enter your Name: ')
+
+last_name = input('Enter your last Name: ')
+
+age = input('Enter yout Age: ')
+
+country = input('Enter your country: ')
+
+city = input('Enter yout City: ')
+
+favourite_car = input('Enter your favourite Car: ')
+
+favourite_colour = input('Enter your favourite colour: ')
+
+print('Hello, my name is ' + name + ". " + 'My last name is ' + last_name  + ". " + 'I am ' + age  + " " + "Years Old. " + "I am from " + country + ". " + 'I live in Capital of Georgia, ' + city + ". " + "My favourite car is " + favourite_car + "," + ' Also My favourite color is ' + favourite_colour + "." )
+ 
+
+x = int(input("Enter x: "))
+
+y = int(input("Enter Y: "))
+
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+
---
+++ Level 7/HomeWork/HomeWork.py
@@ -0,0 +1,105 @@
+
+
+name = input('Enter your Name: ')
+
+last_name = input('Enter your last Name: ')
+
+age = input('Enter yout Age: ')
+
+country = input('Enter your country: ')
+
+city = input('Enter yout City: ')
+
+favourite_car = input('Enter your favourite Car: ')
+
+favourite_colour = input('Enter your favourite colour: ')
+
+print('Hello, my name is ' + name + ". " + 'My last name is ' + last_name  + ". " + 'I am ' + age  + " " + "Years Old. " + "I am from " + country + ". " + 'I live in Capital of Georgia, ' + city + ". " + "My favourite car is " + favourite_car + "," + ' Also My favourite color is ' + favourite_colour + "." )
+
+
+
+x = int(input('Enter X:'))
+y = int(input('Enter Y:'))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+
+x = int(input('Enter X:'))
+y = int(input('Enter Y:'))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+
+x = int(input('Enter X:'))
+y = int(input('Enter Y:'))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+
+x = int(input('Enter X:'))
+y = int(input('Enter Y:'))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+
+x = int(input('Enter X:'))
+y = int(input('Enter Y:'))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+
+x = int(input('Enter X:'))
+y = int(input('Enter Y:'))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+
+x = int(input('Enter X:'))
+y = int(input('Enter Y:'))
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+
+
+
+
+
+
+
+# Int - ფუნქცია, რომლის დანიშნულებაა სტრინგი გადაიყვანოის ინტეჯერში, ანუ რიცხვით მნიშვნელობაში.
+
+# Str - ფუნქცია, რომლის დამიშმულებაა ინტეჯერი გადაიყვანოს სტრინგში
+
+# type - ფუნქცია, რომელიც გვიჩვენებს და გვაძლებს შესაძლებლობას ვნახოთ, გადავამოწმოთ თუ როგორი სახით და ტიპით ვღებულობთ საბოლოო პასუხს.
+
+# Input -  ფუნქცია, რომელიც რომელსაც ვიყენებთ, როცა გვინდა შევატანინოთ მომხმარებელს ესა-თუ ის მნიშვნელობა ცვლადისთვის.
+
+
+
