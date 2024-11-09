8650dc8122cd76ae6d440d1ae1c378ff1c5392a8
davitshavidze <onlygameing61@gmail.com>
2024-11-09 11:57:47 +0400

    second commit

---
+++ Level 9/Classwork/Class.py
@@ -0,0 +1,36 @@
+
+# work 1 
+
+Car = input("Enter car name: ")
+print(type(Car))
+
+y = float(input("Enter y: "))
+print(type(y))
+
+x = int(input("Enter x: "))
+print(type(x))
+
+
+# work 2 
+
+num1 = int(input("Enter first number: "))
+
+num2 = int(input("Enter second number: "))
+
+print(num1 + num2)
+
+
+num3 = int(input("Enter first number: "))
+
+num4 = int(input("Enter second number: "))
+
+print(num3 - num4)
+
+
+num5 = int(input("Enter first number: "))
+
+num6 = int(input("Enter second number: "))
+
+print(num5 * num6)
+
+
---
+++ Level 9/Homework/Homework 1.py
@@ -0,0 +1,84 @@
+
+
+car = "BMW M5 F90"
+
+number1 = int("14")
+
+Class = float("23")
+
+print(type(number1))
+
+print(type(car))
+
+print(type(Class))
+
+
+print(10 > 5)
+print(17 > 12)
+print(3 > 4)
+print(93 > 77)
+print(30 > 32)
+
+
+print(7 < 12)
+print(39 < 19)
+print(77 < 78)
+print(101 < 77)
+print(20 < 89)
+
+
+print(92 >= 99)
+print(21 >= 19)
+print(77 >= 78)
+print(39 >= 39)
+print(77 >= 101)
+
+
+print(7 <= 23)
+print(39 <= 11)
+print(79 <= 78)
+print(231 <= 93)
+print(20 <= 19)
+
+
+print(72 != 72)
+print(39 != 11)
+print(101 != 101)
+print(0 != 0)
+print(20 != 19)
+
+
+print(True and False)
+print(20 and 90)
+print(30 and 81)
+print(88 and 34)
+print(24 and 89)
+print(25 and 68)
+
+
+print(True or False)
+print(80 or 54)
+print(49 or 39)
+print(33 or 91)
+print(121 or 123)
+print(149 or 130)
+
+
+
+Age = str(input("Enter your age:"))
+print(type(Age))
+
+Age = str(input("Enter your age:"))
+print(type(int(Age)))
+
+Age = str(input("Enter your age:"))
+print(type(float(Age)))
+
+
+user_name = input("Enter your User_Name:")
+password = input("Enter your password: ")
+
+print(user_name != user_name)
+print(password != password)
+
+# Concatination - კონკატინაცია - ორი სტრინგის ერთმანეთთან დამატება, შერწყმა.
